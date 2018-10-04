70ab4d6269e09c7282756e75c08ca182abc0169b
Luke <31931505+l-schultz3@users.noreply.github.com>
2018-10-04 13:14:44 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.clearRect(0,0,canvas.width,canvas.height);
+var id = setInterval(frame, 10);
 
-document.getElementById("canvas").addEventListener("mousedown", function(evt) {
-	var mousePos = getMousePos(canvas, evt);
-	mouseDown(mousePos);
-});
+let scale = 40; //size of each cell
+let size = 640; //side length of grid
+let cells = size / scale; //number of cells on side
 
-let scale = 40;
-let maxMines = 40;
-let currentMines = 0;
+let numberOfMines = 40; //the total number of mines that need to placed on board
+let currentNumberOfMines = 0; //the current number of mines placed on board
+
+let masterCellArray = [];
+let playerCellArray = [];
 
 var one = new Image();
 var two = new Image();
@@ -21,166 +22,109 @@ var six = new Image();
 var seven = new Image();
 var eight = new Image();
 
-let segArray = [];
-let shownArray = [];
-
-let tiley;
-let tilex;
-
-var i;
-var j;
-for (i = 0; i < 16; i++) {
-	segArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-	shownArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+function loopThroughEveryCell(array) {
+	for (let i = 0; i < cells; i++) {
+		for (let j = 0; j < cells; j++) {
+		}
+	}
 }
 
-showArray();
+function initializeArray(array, arraySize) { //function used to set arrays to full size needed for game
+	for (let i = 0; i < arraySize; i++) {
+		array.push([]);
+		for (let j = 0; j < cells; j++) {
+			array[i].push(0);
+		}
+	}
+}
 
-while (currentMines < maxMines) {
-	for (i = 0; i < segArray.length; i++) {
-		for (j = 0; j < segArray[i].length; j++) {
-			if (segArray[i][j] === 0) {
-				let posMines = Math.random();
-				if (posMines < 0.1 && currentMines < maxMines) {
-					segArray[i][j] = 9;
-					currentMines += 1;
+function placeMines() {
+	while (currentNumberOfMines < numberOfMines) {
+		for (let i = 0; i < cells; i++) {
+			for (let j = 0; j < cells; j++) {
+				if (Math.random() < 0.1) {
+					masterCellArray[i][j] = 9;
+					currentNumberOfMines++;
 				}
 			}
 		}
 	}
 }
 
-for (i = 0; i < segArray.length; i++) {
-	for (j = 0; j < segArray[i].length; j++) {
-		if (segArray[i][j] < 9) {
-			checkAdjacent(i, j);
+function checkAdjacent() {
+	for (let i = 0; i < cells; i++) {
+		for (let j = 0; j < cells; j++) {
+			checkCellsAdjacent(i, j, i-1, j-1);
+			checkCellsAdjacent(i, j, i-1, j);
+			checkCellsAdjacent(i, j, i-1, j+1);
+			checkCellsAdjacent(i, j, i, j-1);
+			checkCellsAdjacent(i, j, i, j+1);
+			checkCellsAdjacent(i, j, i+1, j-1);
+			checkCellsAdjacent(i, j, i+1, j);
+			checkCellsAdjacent(i, j, i+1, j+1);
 		}
 	}
 }
+
+function checkCellsAdjacent(y, x, checky, checkx) {
+	try {
+		if (masterCellArray[y][x] != 9) {
+			if (masterCellArray[checky][checkx] === 9) {
+				masterCellArray[y][x]++;
+			}
+		}
+	} catch { }
+}
+
+initializeArray(masterCellArray, cells);
+initializeArray(playerCellArray, cells);
+placeMines();
+checkAdjacent();
+
+console.log(masterCellArray);
+
+playerCellArray = masterCellArray;
+
+showArray();
 
 function showArray() {
-//ctx.clearRect(0,0,canvas.width,canvas.height);
-var img = new Image();
-img.onload = function () {
-	for (i = 0; i < shownArray.length; i++) {
-		for (j = 0; j < shownArray[i].length; j++) {
-			if (shownArray[i][j] === 0) {
-				ctx.beginPath();
-				ctx.fillStyle = 'gray';
-				ctx.fillRect(j*scale,i*scale,scale,scale);
-				ctx.fillStyle = 'black';
-				ctx.fillRect(j*scale,i*scale,scale-2,scale-2)
-			} else if (shownArray[i][j] === 13) {
-				ctx.beginPath();
-				ctx.fillStyle = 'white';
-				ctx.fillRect(j*scale,i*scale,scale,scale);
-			} else if (shownArray[i][j] === 9) {
-				ctx.drawImage(img, j*scale, i*scale, scale, scale);
-			} /*else if (shownArray[i][j] === 13) {
-				checkEmpty(i, j);
-			}*/
-		}
-	}
-}
-img.src = "images/mine.png";
-
-showNumber(one, 1, "images/1.png");
-showNumber(two, 2, "images/2.png");
-showNumber(three, 3, "images/3.png");
-showNumber(four, 4, "images/4.png");
-showNumber(five, 5, "images/5.png");
-showNumber(six, 6, "images/6.png");
-showNumber(seven, 7, "images/7.png");
-showNumber(eight, 8, "images/8.png");
-
-}
-
-console.log(segArray);
-
-function checkAdjacent(tiley, tilex) { //function to check how many mines are around a given segment
-	if (tiley - 1 >= 0) {
-		if (tilex - 1 >= 0) {
-			if (segArray[tiley-1][tilex-1] === 9) {
-				segArray[tiley][tilex] += 1;
-			}
-		}
-		if (segArray[tiley-1][tilex] === 9) {
-			segArray[tiley][tilex] += 1;
-		}
-		if (tilex + 1 < 16) {
-			if (segArray[tiley-1][tilex+1] === 9) {
-				segArray[tiley][tilex] += 1;
+	let img = new Image();
+	img.onload = function () {
+		for (i = 0; i < playerCellArray.length; i++) {
+			for (j = 0; j < playerCellArray[i].length; j++) {
+				if (playerCellArray[i][j] === 0) {
+					ctx.beginPath();
+					ctx.fillStyle = 'gray';
+					ctx.fillRect(j*scale,i*scale,scale,scale);
+					ctx.fillStyle = 'black';
+					ctx.fillRect(j*scale,i*scale,scale-2,scale-2)
+				} else if (playerCellArray[i][j] === 13) {
+					ctx.beginPath();
+					ctx.fillStyle = 'white';
+					ctx.fillRect(j*scale,i*scale,scale,scale);
+				} else if (playerCellArray[i][j] === 9) {
+					ctx.drawImage(img, j*scale, i*scale, scale, scale);
+				}
 			}
 		}
 	}
+	img.src = "images/mine.png";
 
-	if (tilex - 1 >= 0) {
-		if (segArray[tiley][tilex-1] === 9) {
-			segArray[tiley][tilex] += 1;
-		}
-	}
-
-	if (tilex + 1 < 16) {
-		if (segArray[tiley][tilex+1] === 9) {
-			segArray[tiley][tilex] += 1;
-		}
-	}
-
-	if (tiley + 1 < 16) {
-		if (tilex - 1 >= 0) {
-			if (segArray[tiley+1][tilex-1] === 9) {
-				segArray[tiley][tilex] += 1;
-			}
-		}
-		if (segArray[tiley+1][tilex] === 9) {
-			segArray[tiley][tilex] += 1;
-		}
-		if (tilex + 1 < 16) {
-			if (segArray[tiley+1][tilex+1] === 9) {
-				segArray[tiley][tilex] += 1;
-			}
-		}
-	}
-
-	if (segArray[tiley][tilex] === 0) {
-		segArray[tiley][tilex] = 13;
-	}
-
-//the reason for all of these if statement is because if the segment is on an edge the program crashes because it called an array out of bounds
-}
-
-function mouseDown(mousePos) {
-//console.log('Mouse position: ' + (mousePos.x) + ',' + (mousePos.y));
-//console.log('Seg position: ' + Math.floor(mousePos.x / scale) + ',' + Math.floor(mousePos.y / scale));
-	shownArray[Math.floor(mousePos.y / scale)][Math.floor(mousePos.x / scale)] = segArray[Math.floor(mousePos.y / scale)][Math.floor(mousePos.x / scale)];
-	if (segArray[Math.floor(mousePos.y / scale)][Math.floor(mousePos.x / scale)] === 13) {
-		revealEmpty(Math.floor(mousePos.y / scale), Math.floor(mousePos.x / scale));
-	}
-	checkEmpty(Math.floor(mousePos.y / scale), Math.floor(mousePos.x / scale));
-
-	tiley = Math.floor(mousePos.y / scale);
-	tilex = Math.floor(mousePos.x / scale);
-
-	checkEmpty(tiley, tilex);
-
-
-
-	showArray();
-}
-
-function getMousePos(canvas, evt) {
-	var rect = canvas.getBoundingClientRect();
-	return {
-		x: evt.clientX - rect.left,
-		y: Math.round(evt.clientY - rect.top)
-	};
+	showNumber(one, 1, "images/1.png");
+	showNumber(two, 2, "images/2.png");
+	showNumber(three, 3, "images/3.png");
+	showNumber(four, 4, "images/4.png");
+	showNumber(five, 5, "images/5.png");
+	showNumber(six, 6, "images/6.png");
+	showNumber(seven, 7, "images/7.png");
+	showNumber(eight, 8, "images/8.png");
 }
 
 function showNumber(image, num, source) {
 	image.onload = function() {
-		for (i = 0; i < shownArray.length; i++) {
-			for (j = 0; j < shownArray[i].length; j++) {
-				if (shownArray[i][j] === num) {
+		for (i = 0; i < playerCellArray.length; i++) {
+			for (j = 0; j < playerCellArray[i].length; j++) {
+				if (playerCellArray[i][j] === num) {
 					ctx.drawImage(image, j*scale, i*scale, scale, scale);
 				}
 			}
@@ -189,72 +133,21 @@ function showNumber(image, num, source) {
 	image.src = source;
 }
 
-function revealEmpty(tiley, tilex) { //function to check how many mines are around a given segment
-	if (tiley - 1 >= 0) {
-		if (tilex - 1 >= 0) {
-			shownArray[tiley - 1][tilex - 1] = segArray[tiley - 1][tilex - 1];
-		}
-		shownArray[tiley - 1][tilex] = segArray[tiley - 1][tilex];
-		if (tilex + 1 < 16) {
-			shownArray[tiley - 1][tilex + 1] = segArray[tiley - 1][tilex + 1];
-		}
-	}
-
-	if (tilex - 1 >= 0) {
-		shownArray[tiley][tilex - 1] = segArray[tiley][tilex - 1];
-	}
-
-	if (tilex + 1 < 16) {
-		shownArray[tiley][tilex + 1] = segArray[tiley][tilex + 1];
-	}
-
-	if (tiley + 1 < 16) {
-		if (tilex - 1 >= 0) {
-			shownArray[tiley + 1][tilex - 1] = segArray[tiley + 1][tilex - 1];
-		}
-		shownArray[tiley + 1][tilex] = segArray[tiley + 1][tilex];
-		if (tilex + 1 < 16) {
-			shownArray[tiley + 1][tilex + 1] = segArray[tiley + 1][tilex + 1];
-		}
-	}
-
-//the reason for all of these if statement is because if the segment is on an edge the program crashes because it called an array out of bounds
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: Math.round(evt.clientY - rect.top)
+  };
 }
 
-function checkEmpty(tiley, tilex) {
+canvas.addEventListener('mousemove', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  //console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+}, false);
 
-	let newTiley = tiley;
-	let newTilex = tilex;
-
-	while (newTiley < 15 && segArray[newTiley][newTilex] === 13) {
-		revealEmpty(newTiley, newTilex);
-		newTiley+= 1;
-	}
-
-	newTiley = tiley;
-	newTilex = tilex;
-
-	while (newTiley > 0 && segArray[newTiley][newTilex] === 13) {
-		revealEmpty(newTiley, newTilex);
-		newTiley -= 1;
-	}
-
-	newTiley = tiley;
-	newTilex = tilex;
-
-	while (newTilex < 15 && segArray[newTiley][newTilex] === 13) {
-		revealEmpty(newTiley, newTilex);
-		newTilex += 1;
-	}
-
-	newTiley = tiley;
-	newTilex = tilex;
-	
-	while (newTilex > 0 && segArray[newTiley][newTilex] === 13) {
-		revealEmpty(newTiley, newTilex);
-		newTilex -= 1;
-	}
-
-	//shownArray[tiley][tilex] = 15;
-	console.log(shownArray);
+function frame() {
+//location.reload();
 }
+
+//hi luke, it's maher, I helped you in this project, better give me credit when you hand it in. Mercer you're gonna read this sooo you're a witness.
